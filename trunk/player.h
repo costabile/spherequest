61cd83d8f3826088a1fc76fe7c5e2a7ad951ffc4
smith.kaitlin <smith.kaitlin@9544383c-15c0-11df-8b40-97b39cf34117>
2010-03-03 01:38:20 +0000

    Started a player.h header where we can keep function prototypes related to the player's progress.  For now it has two variable declarations in it, but I'm not sure if there's really a point to this, so we can remove that in the future.  Variables can't be initialized in a header.

---
+++ trunk/player.h
@@ -0,0 +1,12 @@
+//SphereQuest
+//Game Project for SW Eng 3GC3
+//Written by Jason Costabile, Ben Kybartas, Kaitlin Smith
+//player.h -- stores information based on the player's progress
+
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+extern static int zen;
+extern static int level;
+
+#endif
