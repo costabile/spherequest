81f7cb6e56c1e531b1a3647f52b5c1c183811802
smith.kaitlin <smith.kaitlin@9544383c-15c0-11df-8b40-97b39cf34117>
2010-03-06 22:19:06 +0000

    Introduced a draw class.  Leaving the drawSphere method out for now, because it depends on a lot of variables that are used elsewhere.

---
+++ draw.h
@@ -0,0 +1,25 @@
+// draw.h
+// Contains functions relating to drawing commands.
+
+#ifndef DRAW_H
+#define DRAW_H
+
+#include <stdlib.h>
+#include <glut.h>
+
+class draw {
+public:
+	draw(); // Constructor
+	~draw(){}; // Destructor
+	//void drawSphere();
+	void drawTemples();
+	void drawXWall();
+	void drawZWall();
+	void drawTree();
+	void drawWiseMen();
+	void drawGround();
+	void drawSkybox();
+	void drawMtFuji();
+};
+
+#endif
--- player.cpp
+++ player.cpp
@@ -1,2 +1,3 @@
 //player.cpp
 //Code involved with keeping track of the player's progress will go here.
+
