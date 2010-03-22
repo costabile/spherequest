c01b03727f093f437c2f96d47d123061eff581f3
bkybartas <bkybartas@9544383c-15c0-11df-8b40-97b39cf34117>
2010-03-22 16:11:26 +0000

    Added a class to declare riddles for the wisemen. Still need to create a ChallengeList class.

---
+++ challenge.h
@@ -0,0 +1,31 @@
+// SphereQuest - Software Engineering 3GC3 Game Project
+// By: Ben Kybartas, Kaitlin Smith and Jason Costabile
+
+// A Class which defines a challenge of the wisemen.
+#ifndef CHALLENGE_H
+#define CHALLENGE_H
+
+#include <string>
+
+class challenge{
+public:
+	challenge(std::string question, std::string a, std::string b, std:: string c, int correctans); //constructor
+	
+	// Definition question = question to be asked
+	//				a = answer a
+	//				b = answer b
+	//				c = answer c
+	//				correctans = correct answer
+	//						 0 = a
+	//						 1 = b
+	//						 2 = c
+	~challenge(){}; //destructor
+	bool checkAnswer(int choice);
+	std::string correctAnswer();
+	std::string getQuestion();
+	std::string getA();
+	std::string getB();
+	std::string getC();
+
+};
+#endif
