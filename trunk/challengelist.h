2d1239e794c3495141c4545b6e26363a5df04361
constabile <constabile@9544383c-15c0-11df-8b40-97b39cf34117>
2010-04-04 22:00:45 +0000

    Got the challengelist working properly! I ended up just removing the challenge class entirely and keeping all the info in separate arrays inside of challengelist. Also added a couple methods (get/set challengeNum) to facilitate saving/loading.

--- trunk/challengelist.h
+++ trunk/challengelist.h
@@ -6,8 +6,6 @@
 #ifndef CHALLENGELIST_H
 #define CHALLENGELIST_H
 
-#include "challenge.h"
-
 class challengelist{
 
 public:
@@ -19,8 +17,7 @@ public:
 	affect the challenge list although the challenge list is designed to
 	work on its own
 	*/
-	challenge getChallenge(int number); // Get a specific challenge
-	challenge getNextChallenge(); //Get the actual challenge object
+	void setChallengeNum(int newNum);
 	void incrementChallengeNum(); //manually go to the next challenge
 	void decrementChallengeNum(); //manually go to the previous challenge
 
@@ -28,6 +25,7 @@ public:
 	These operators should be used since they are abstract and require no
 	manual changes to be made to the challenge list
 	*/
+	int getChallengeNum();		//returns the challenge number
 	char * getCurrentChallenge(); // Get a string of the current challenge
 	char * getFirstChoice(); // Get the first choice (a)
 	char * getSecondChoice(); // Get the second choice (b)
