5eec863c2ad70116286901ddc6d6ae1c1f6f40f4
smith.kaitlin <smith.kaitlin@9544383c-15c0-11df-8b40-97b39cf34117>
2010-04-04 02:24:13 +0000

    Added 10 riddles.  They're silly.  Also commented out the zen variable in GameBase.cpp because it's already defined in player.cpp. Not sure which one we want to keep yet.

--- trunk/challengelist.cpp
+++ trunk/challengelist.cpp
@@ -5,13 +5,22 @@
 
 using namespace std;
 
-challenge challenges[2];
+challenge challenges[6];
 int challengenum;
 
 challengelist::challengelist(){
+	// Riddles are from this website: http://inspirationalriddles.blogspot.com/
 	challengenum = 0;
-	challenges[0] = challenge("What is 1 + 1?", "2", "3", "4", 0);
-	challenges[1] = challenge("What is 3 * 3?", "6", "9", "12", 1);
+	challenges[0] = challenge("What has both light and darkness?", "The mind", "The heart", "A room", 0);
+	challenges[1] = challenge("What is the one thing that man will always surrender to?", "Bears", "His appetite", "Fear", 1);
+	challenges[2] = challenge("What is the fastest way to show happiness and sadness?", "A smile and a tear", "A giggle and a frown", "A grin and a cry", 0);
+	challenges[3] = challenge("Who is small and can give much more than she gave as a maiden?", "A rock", "A fruit", "A seed", 2);
+	challenges[4] = challenge("This could be many things but is only one.  It leaves right away if you wait for it to return.  What is it?", "A train", "Love", "The present", 2);
+	challenges[5] = challenge("I will tell you what nobody ever did and nobody ever can.  What am I?", "Silence", "Memories", "Dreams", 0);
+	challenges[6] = challenge("I know you better than anyone else, and if your smile is true.  What am I?", "Your toothbrush", "The mirror", "Your mind", 1);
+	challenges[7] = challenge("We have been together since the day you were born and you don't have a place without me. What am I?", "Your life", "Your spirit", "Your name", 2);
+	challenges[8] = challenge("I accept you during the day but I don't tolerate you at night. What is it?", "Insomnia", "Hunger", "Fear", 0);
+	challenges[9] = challenge("My hands are small but contain the unsurpassable eternity. What am I?", "A child", "A clock", "A flower", 1);
 }
 
 challenge challengelist::getChallenge(int number){
